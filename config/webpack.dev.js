ade40f395601e46654c3d42122a6c48f2c3abc27
KangAlexey <Kan030196!>
2018-05-21 17:40:19 +0600

    All files worked good

---
+++ config/webpack.dev.js
@@ -0,0 +1,11 @@
+const merge = require('webpack-merge');
+const common = require('../webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devServer: {
+        host: 'localhost',
+        port: 3001,
+        open: true,
+    }
+});
